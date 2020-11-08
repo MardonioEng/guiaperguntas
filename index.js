dd482200c0f6d07a98673f4f125eb805d00efe3a
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-08 16:07:52 -0300

    Adicionando o Bootstrap

--- index.js
+++ index.js
@@ -4,17 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let exibirMsg = true;
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Dev Marodrigues",
-        inscritos: 8001,
-        msg: exibirMsg,
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(3000, ()=>{console.log("App rodando...")});
