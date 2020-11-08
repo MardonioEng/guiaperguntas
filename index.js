700c7e612500b15d4a6397aa06022f60dceec940
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-08 15:11:02 -0300

    Passagem de variável para HTML

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:nome/:lang", (req, res) => {
+    let nome = req.params.nome;
+    let lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Dev Marodrigues",
+        inscritos: 8001
+    });
 });
 
 app.listen(3000, ()=>{console.log("App rodando...")});
