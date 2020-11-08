b2ffa0490526734df746a81bd32cfe940e439296
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-08 19:41:44 -0300

    Criãção da View Perguntar

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.render("index");
+});
+
+app.get('/perguntar', (req, res) => {
+    res.render("perguntar");
 });
 
 app.listen(3000, ()=>{console.log("App rodando...")});
