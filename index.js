87031ff44f2a337603ef22c4d62463a607686fd3
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-08 15:55:57 -0300

    Configurando o uso de arquivos estáticos

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/:nome/:lang", (req, res) => {
     let nome = req.params.nome;
     let lang = req.params.lang;
+    let exibirMsg = true;
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Dev Marodrigues",
-        inscritos: 8001
+        inscritos: 8001,
+        msg: exibirMsg,
     });
 });
 
