18e8d0965359d0dfcc0a9e61a4535f08de4137db
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-09 08:41:06 -0300

    Conexão com o BD mysql

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require('./database/database');
 
+// database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão feita com o banco de dados!')
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// Configurações
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Rotas
 app.get('/', (req, res) => {
     res.render("index");
 });
