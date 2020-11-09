a6db241c8e1cb5e611b7ffb0c2b422ef22c33a7c
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-09 08:57:25 -0300

    Criação model Pergunta

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require('./database/database');
+const perguntaModel = require('./database/Pergunta');
 
 // database
-
 connection
     .authenticate()
     .then(() => {
