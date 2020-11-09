18acbc7477001a6878986051430ad376651af74d
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-09 08:14:16 -0300

    Capturando dados do formulário com body-parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render("index");
@@ -10,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/perguntar', (req, res) => {
     res.render("perguntar");
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send('Formulário Recebido! '+titulo+ " " + descricao);
 });
 
 app.listen(3000, ()=>{console.log("App rodando...")});
